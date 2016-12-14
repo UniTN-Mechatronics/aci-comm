87897e83a24a2dff719833a157c7ea1287b40f0c
MatteoRagni <matteo.ragni.it@gmail.com>
2016-12-15 00:01:32 +0100

    Angular rate and accelerations with orientation

--- include/uav_frame.hpp
+++ include/uav_frame.hpp
@@ -7,10 +7,10 @@
 #include "conversion_lambda.hpp"
 #include "uav_commons.hpp"
 
-namespace acc 
+namespace acc
 {
     template<class T, class FloatingPointPrecision>
-    class Angles 
+    class Angles
     {
     private:
 
@@ -20,12 +20,22 @@ namespace acc
             roll.ChannelRead<T,   FloatingPointPrecision>::_uav_ptr = uav_ptr;
             roll.ChannelWrite<T,  FloatingPointPrecision>::_uav_ptr = uav_ptr;
             yaw.ChannelRead<T,    FloatingPointPrecision>::_uav_ptr = uav_ptr;
+
+            d_pitch.ChannelRead<T,  FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            d_pitch.ChannelWrite<T, FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            d_roll.ChannelRead<T,   FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            d_roll.ChannelWrite<T,  FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            d_yaw.ChannelRead<T,    FloatingPointPrecision>::_uav_ptr = uav_ptr;
+
+            dd_x.ChannelRead<T,    FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            dd_y.ChannelRead<T,    FloatingPointPrecision>::_uav_ptr = uav_ptr;
+            dd_z.ChannelRead<T,    FloatingPointPrecision>::_uav_ptr = uav_ptr;
         };
 
         /*
-        *    __          
-        *   |__).|_ _|_  
-        *   |   ||_(_| ) 
+        *    __
+        *   |__).|_ _|_
+        *   |   ||_(_| )
         */
         template<class TP, class ReturnType, class ArgsType>
         class Pitch: public virtual ChannelRead<TP, ReturnType>, public virtual ChannelWrite<TP, ArgsType>
@@ -35,26 +45,55 @@ namespace acc
 
             Pitch() {
                 ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::angle_pitch;
-                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::CTRL_pitch;
+                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::CTRL_pitch; // TODO Check map value... DMC_pitch?
             };
 
         protected:
-            ReturnType 
+            ReturnType
             _read_conversion(int value) {
                 return angle_read_conv(value);
             }
 
-            int 
+            int
             _write_conversion(ReturnType value) {
-                return (int)value;
+                return CTRL_pitch_roll_write_conv(value);
+            }
+        };
+
+        template<class TP, class ReturnType, class ArgsType>
+        class PitchDot: public virtual ChannelRead<TP, ReturnType>, public virtual ChannelWrite<TP, ArgsType>
+        {
+        public:
+            friend class Angles;
+
+            PitchDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::angvel_pitch;
+                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::DMC_pitch; // TODO Check map value... CTRL_pitch?
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return angvel_read_conv(value);
+            }
+
+            int
+            _write_conversion(ReturnType value) {
+                return DMC_angles_write_conv(value);
+            }
+
+            void
+            _ctrl_mode_check() {
+                if (ChannelWrite<TP, FloatingPointPrecision>::_uav_ptr->_ctrl_mode == CTRL_MODE::DMC) return;
+                throw std::runtime_error("Pitch rate can be controlled only in DMC mode");
             }
         };
 
 
         /*
-        *    __      
-        *   |__)_ || 
-        *   | \(_)|| 
+        *    __
+        *   |__)_ ||
+        *   | \(_)||
         */
         template<class TP, class ReturnType, class ArgsType>
         class Roll: public virtual ChannelRead<TP, ReturnType>, public virtual ChannelWrite<TP, ArgsType>
@@ -64,25 +103,57 @@ namespace acc
 
             Roll() {
                 ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::angle_roll;
-                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::CTRL_roll;
+                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::CTRL_roll; // TODO Check map value... DMC_roll?
             };
 
         protected:
-            ReturnType 
+            ReturnType
             _read_conversion(int value) {
-                return angle_read_conv(value);
+                return angle_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO Check if correct
             }
 
-            int 
+            int
             _write_conversion(ReturnType value) {
-                return (int)value;
+                return CTRL_pitch_roll_write_conv(value *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation)); // TODO Check if correct
             }
         };
 
+        template<class TP, class ReturnType, class ArgsType>
+        class RollDot: public virtual ChannelRead<TP, ReturnType>, public virtual ChannelWrite<TP, ArgsType>
+        {
+        public:
+            friend class Angles;
+
+            RollDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::angvel_roll;
+                ChannelWrite<TP, ReturnType>::_write_type = ACI_COMM_CMD::DMC_roll; // TODO Check map value... CTRL_pitch?
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return angvel_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO check if correct
+            }
+
+            int
+            _write_conversion(ReturnType value) {
+                return DMC_angles_write_conv(value *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation)); // TODO check if correct
+            }
+
+            void
+            _ctrl_mode_check() {
+                if (ChannelWrite<TP, FloatingPointPrecision>::_uav_ptr->_ctrl_mode == CTRL_MODE::DMC) return;
+                throw std::runtime_error("Roll rate can be controlled only in DMC mode");
+            }
+        };
 
         /*
-        *   \_/_     
-        *    |(_|\)/ 
+        *   \_/_
+        *    |(_|\)/
         */
         template<class TP, class ReturnType>
         class Yaw: public virtual ChannelRead<TP, ReturnType>
@@ -95,9 +166,86 @@ namespace acc
             };
 
         protected:
-            ReturnType 
+            ReturnType
             _read_conversion(int value) {
-                return angle_read_conv(value);
+                return angle_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO check if correct
+            }
+        };
+
+        template<class TP, class ReturnType>
+        class YawDot: public virtual ChannelRead<TP, ReturnType> // TODO And write?? DMC, CTRL?? not written for now
+        {
+        public:
+            friend class Angles;
+
+            YawDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::angvel_yaw;
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return angvel_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO check if correct
+            }
+        };
+
+        //     _             _              _   _
+        //    /_\  __ __ ___| |___ _ _ __ _| |_(_)___ _ _  ___
+        //   / _ \/ _/ _/ -_) / -_) '_/ _` |  _| / _ \ ' \(_-<
+        //  /_/ \_\__\__\___|_\___|_| \__,_|\__|_\___/_||_/__/
+
+        template<class TP, class ReturnType>
+        class XDotDot: public virtual ChannelRead<TP, ReturnType>
+        {
+        public:
+            friend class Angles;
+
+            XDotDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::acc_x;
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return acc_read_conv(value);
+            }
+        };
+
+        template<class TP, class ReturnType>
+        class YDotDot: public virtual ChannelRead<TP, ReturnType>
+        {
+        public:
+            friend class Angles;
+
+            YDotDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::acc_y;
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return acc_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO check if correct
+            }
+        };
+
+        template<class TP, class ReturnType>
+        class ZDotDot: public virtual ChannelRead<TP, ReturnType>
+        {
+        public:
+            friend class Angles;
+
+            ZDotDot() {
+                ChannelRead<TP, ReturnType>::_read_type = ACI_COMM_VAR::acc_z;
+            };
+
+        protected:
+            ReturnType
+            _read_conversion(int value) {
+                return acc_read_conv(value) *
+                  static_cast<ReturnType>(ChannelRead<TP, ReturnType>::_uav_ptr->orientation); // TODO check if correct
             }
         };
 
@@ -106,6 +254,15 @@ namespace acc
         Yaw<T, FloatingPointPrecision> yaw;
         Roll<T, FloatingPointPrecision, FloatingPointPrecision> roll;
         Pitch<T, FloatingPointPrecision, FloatingPointPrecision> pitch;
+
+        YawDot<T, FloatingPointPrecision> d_yaw;
+        RollDot<T, FloatingPointPrecision, FloatingPointPrecision> d_roll;
+        PitchDot<T, FloatingPointPrecision, FloatingPointPrecision> d_pitch;
+
+        XDotDot<T, FloatingPointPrecision> dd_x;
+        YDotDot<T, FloatingPointPrecision> dd_y;
+        ZDotDot<T, FloatingPointPrecision> dd_z;
+
         Angles() {};
     };
 
@@ -113,4 +270,3 @@ namespace acc
 
 #endif // __cplusplus
 #endif // _ACI_COMM_UAV_FRAME_HPP_
-
